69a10b6eab3d43c733dfc533e9a6cfa1ca3f38be
icedteafrog <63257773+icedteafrog@users.noreply.github.com>
2021-07-01 21:16:31 +0200

    Update SuggestCommand.js

--- src/commands/administration/SuggestCommand.js
+++ src/commands/administration/SuggestCommand.js
@@ -1,23 +1,34 @@
-const BaseCommand = require('../../utils/structures/BaseCommand');
-const Discord = require('discord.js');
+const BaseCommand = require( '../../utils/structures/BaseCommand' );
+const Discord = require( 'discord.js' );
 
-module.exports = class SuggestCommand extends BaseCommand {
-  constructor() {
-    super('suggest', 'administration', []);
-  }
+module.exports = class SuggestCommand extends BaseCommand
+{
+    constructor()
+    {
+        super( 'suggest', 'administration', [] );
+    }
 
-  async run(client, message, args) {
+    async run( client, message, args )
+    {
 
-    let sug = args.slice(0).join(" ");
+        let sug = args.slice( 0 )
+            .join( " " );
 
-    if (!sug) return message.channel.send('No suggestion given, please try again. (e.g. ?suggest suggestion)')
+        if ( !sug ) return message.channel.send(
+            'No suggestion given, please try again. (e.g. ?suggest suggestion)'
+        )
 
-    const Embed = new Discord.MessageEmbed()
-    .setTitle(`Suggestion: ${sug}`)
-    .setAuthor(`Suggested by ${message.author.username}`)
-    .setColor('#007ACC')
-    .setFooter('Developed by snow#0911', message.author.displayAvatarURL({ dynamic : true}))
+        const Embed = new Discord.MessageEmbed()
+            .setTitle( `Suggestion: ${sug}` )
+            .setAuthor( `Suggested by ${message.author.username}` )
+            .setColor( '#007ACC' )
+            .setFooter( 'Developed by snow#0911', message.author.displayAvatarURL(
+            {
+                dynamic: true
+            } ) )
 
-    message.channel.send(Embed).then(sentmsg => sentmsg.react('⬆️')).then(reaction => reaction.message.react('⬇️'));
-  }
+        message.channel.send( Embed )
+            .then( sentmsg => sentmsg.react( '⬆️' ) )
+            .then( reaction => reaction.message.react( '⬇️' ) );
+    }
 }
