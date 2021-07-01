28586d6520a43b3dec00059f8bc13ce9f3c576d3
icedteafrog <63257773+icedteafrog@users.noreply.github.com>
2021-07-01 22:04:41 +0200

    Adding comments

--- src/commands/administration/ClearCommand.js
+++ src/commands/administration/ClearCommand.js
@@ -8,6 +8,7 @@ module.exports = class ClearCommand extends BaseCommand
         super( 'clear', 'administration', [] );
     }
 
+    // Simple message if you don't have permissions and you attempt to run this command. You can change it as long as you don't delete the ''
     async run( client, message, args )
     {
         if ( !message.member.permissions.has( "MANAGE_MESSAGES" ) )
@@ -15,6 +16,7 @@ module.exports = class ClearCommand extends BaseCommand
                 'You cannot use this command, it requires `MANAGE_MESSAGES` permissions.'
             );
 
+        // Classic fail message
         if ( !args[ 0 ] ) return message.channel.send(
                 '**Please provide a valid amount of messages to clear.**'
             )
@@ -26,6 +28,7 @@ module.exports = class ClearCommand extends BaseCommand
                 } );
             } )
 
+        // If the value isn't between 2 & 100, the command will not attempt to work
         if ( args[ 0 ] > 100 ) return message.channel.send(
                 `**Value must be between 2 & 100!**` )
             .then( msg =>
@@ -36,6 +39,7 @@ module.exports = class ClearCommand extends BaseCommand
                 } );
             } )
 
+        // Success message. This should be self-explanatory if you aren't dumb
         const Embed = new Discord.MessageEmbed()
             .setAuthor( `Successfully cleared ${args[0]} Messages!` )
             .setColor( '#007ACC' )
@@ -44,6 +48,7 @@ module.exports = class ClearCommand extends BaseCommand
                 dynamic: true
             } ) )
 
+        // Message will be deleted after 7000ms. You can change the value as you want, if you want to change this code to not delete the message absolutely, you can simply delete this part of code as you want (do it only if you know how)
         message.channel.bulkDelete( args[ 0 ] )
             .then( Message =>
             {
